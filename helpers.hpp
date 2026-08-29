35e1effd2767724b80fc617fdf3f230953e3195b
HassanIQ777 <hassaniq793@gmail.com>
2026-08-30 01:53:17 +0300

    Few additions

--- helpers.hpp
+++ helpers.hpp
@@ -10,21 +10,6 @@ inline void LOG(const std::string &msg) {
   File::insertline(globals.files.logs_file, output, 0);
 }
 
-inline void printChoice(const std::string &num, const std::string &msg) {
-  print(color::TXT_YELLOW, num, ") ", color::TXT_GREEN, msg, color::A_RESET,
-        "\n");
-}
-
-inline void printMainMenu() {
-  print("Select choice:\n");
-  printChoice("1", "Video (Best Quality)");
-  printChoice("2", "Video (720p)");
-  printChoice("3", "Video (480p)");
-  printChoice("4", "Video (360p)");
-  printChoice("5", "Audio Only");
-  print("> ");
-}
-
 inline void printHelp(Globals &globals) {
   const std::string program_name = globals.parser.getArg(0);
   print("Usage:\n");

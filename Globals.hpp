34f8640c99de3dc2d9c28517e5bfdf7db38697fd
HassanIQ777 <hassaniq793@gmail.com>
2026-09-22 16:20:47 +0300

    version

--- Globals.hpp
+++ Globals.hpp
@@ -130,7 +130,7 @@ struct FilePaths {
 enum class AppState { MainMenu, Downloading, Settings, Help, Quit };
 
 struct Globals {
-  std::string VERSION = "v26.9.21";
+  std::string VERSION = "v26.9.22";
   FilePaths files;
   CLIParser parser;
   AppState state = AppState::MainMenu;
